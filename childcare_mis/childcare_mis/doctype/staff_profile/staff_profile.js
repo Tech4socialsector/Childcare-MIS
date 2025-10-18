de01840d26773011617fb99d442c78295d5a8207
augustinAPF <augustin.moses@azimpremjifoundation.org>
2025-10-18 18:08:57 +0530

    DocTypes are created

---
+++ childcare_mis/childcare_mis/doctype/staff_profile/staff_profile.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Augustin Moses and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Staff profile", {
+// 	refresh(frm) {
+
+// 	},
+// });
